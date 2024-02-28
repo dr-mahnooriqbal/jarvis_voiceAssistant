a6fc5415ad310529c896decf0e1746b0291c6d2d
Mahnoor Iqbal <mahnoor.iqbal456@gmail.com>
2024-02-29 04:47:41 +0500

    Add files via upload

---
+++ jarvis ahm.py
@@ -0,0 +1,106 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('mahnoor.iqbal456@gmail.com', 'sabrahkhatoon')
+    server.sendmail('mahnoor.iqbal26@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open bano kabil website' in query:
+            speak("opening banoqabil website")
+            webbrowser.open("banoqabil.com")
+
+        elif 'open youtube' in query:
+            speak("opening youtube")
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            speak("opening google")
+            webbrowser.open("google.com")
+
+        elif 'open stack overflow' in query:
+            speak("opening stack overflow")
+            webbrowser.open("stackoverflow.com")   
+
+        elif 'tell me the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Mam, the time is {strTime}")
+
+
+        elif 'send email to mahnoor' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "mahnoor.iqbal26@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry, I am not able to send this email")    
